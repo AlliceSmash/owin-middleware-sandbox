1f2c8b013908a7c42e2e58fd39fae63032791949
sabrina <sabrina@formfire.com>
2015-12-03 15:16:04 -0500

    refactor with extension methods

---
+++ testkatana/AppBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Owin;
+
+namespace testkatana
+{
+    public static class AppBuilderExtensions
+    {
+
+        public static void UseMyMiddleWare(this IAppBuilder app)
+        {
+            app.Use<MyMiddlewareComponent>();
+        }
+
+        public static void UseMyOtherMiddleWare(this IAppBuilder app)
+        {
+            app.Use<MyOtherMiddlewareComponent>();
+        }
+    }
+}
--- testkatana/Startup.cs
+++ testkatana/Startup.cs
@@ -5,17 +5,12 @@ using System.Threading.Tasks;
 
 namespace testkatana
 {
-    using Microsoft.Owin;
-    using System.IO;
-    using AppFunc = Func<IDictionary<string, object>, Task>;
     public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            app.Use<MyMiddlewareComponent>();
-            app.Use<MyOtherMiddlewareComponent>();
+            app.UseMyMiddleWare();
+            app.UseMyOtherMiddleWare();
         }
-
-       
     }
 }
