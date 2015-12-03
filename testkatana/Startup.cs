e0165c92ff57c6c6efbd29cae6ea47fa2ca81a82
sabrina <sabrina@formfire.com>
2015-12-03 14:58:41 -0500

    add second middleware, and it works

--- testkatana/Startup.cs
+++ testkatana/Startup.cs
@@ -12,7 +12,10 @@ namespace testkatana
         public void Configuration(IAppBuilder app)
         {
             var middleware = new Func<AppFunc, AppFunc>(MyMiddleWare);
+            var secondMiddleware = new Func<AppFunc, AppFunc>(MyOtherMiddleWare);
+
             app.Use(middleware);
+            app.Use(secondMiddleware);
         }
 
         public AppFunc MyMiddleWare(AppFunc next)
@@ -27,10 +30,28 @@ namespace testkatana
                 }
                 // Call the next Middleware in the chain:
                 await next.Invoke(environment);
+                using (var writer = new StreamWriter(response))
+                {
+                    await writer.WriteAsync("<h1>After second middleware, say hello from My First Middleware</h1>");
+                }
             };
             return appFunc;
         }
 
-        
+        public AppFunc MyOtherMiddleWare(AppFunc next)
+        {
+            AppFunc appFunc = async (IDictionary<string, object> environment) =>
+            {
+                // Do something with the incoming request:
+                var response = environment["owin.ResponseBody"] as Stream;
+                using (var writer = new StreamWriter(response))
+                {
+                    await writer.WriteAsync("<h1>Hello from My Second Middleware</h1>");
+                }
+                // Call the next Middleware in the chain:
+                await next.Invoke(environment);
+            };
+            return appFunc;
+        }
     }
 }
